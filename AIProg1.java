9fb261e6bbf0071b0a7dc918b86c5d42b98cccac
Chancer <Chancergs@hotmail.com>
2018-10-08 04:57:37 -0600

    Greedy First done, Depth First Almost Done
    Depth First just needs a way to mark the correct path and it should be
    done.

--- AIProg1.java
+++ AIProg1.java
@@ -5,14 +5,15 @@
  */
 package aiprog1;
 
-import java.io.*; 
+import java.io.*;
+import java.util.Stack;
 
 public class AIProg1 {
     private static Node board[][];
     
     
     public static void main(String[] args) throws Exception {
-        FileReader fr = new FileReader("C:\\Users\\Chance\\Documents\\NetBeansProjects\\AIProg1\\src\\aiprog1\\largemaze.txt");
+        FileReader fr = new FileReader("C:\\Users\\Chance\\Documents\\NetBeansProjects\\AIProg1\\src\\aiprog1\\openmaze.txt");
         BufferedReader br = new BufferedReader(fr);
         
         br.mark(10000); //this lets us reset the buffered reader so we can go through a bunch of times.
@@ -45,19 +46,17 @@ public class AIProg1 {
             }
             //System.out.println();
         }
+
+        //greedyBest(board);
+        depthFirst(board);
         
         //This prints the maze, used for debugging.
-        
-        /*
         for(int i = 0; i < board.length - 1; i++){
             for(int j = 0; j < board[0].length - 1; j++){
                 System.out.print(board[i][j].value);
             }
             System.out.println();
         }
-        */
-        
-        greedyBest(board);
     }
 
     public static void aStar(Node[][] board){
@@ -67,6 +66,83 @@ public class AIProg1 {
     public static void depthFirst(Node[][] board){
         //expand deepest unexpanded node
         //Last in, First out.
+        
+        //find start point 
+        int curH = 0;
+        int curV = 0;
+        for(int i = 0; i < board.length - 1; i++){
+            for(int j = 0; j < board[0].length - 1; j++){
+                if(board[i][j].value.equals("P")){
+                    curH = j;
+                    curV = i;
+                }
+            }
+        }
+        
+        Stack frontier = new Stack();
+        frontier.push(board[curV][curH]);
+        
+        int checker = 0;
+        boolean done = false;
+        while(!done){
+            checker++;
+            //board[i-1][j] above
+            //board[i][j+1] right
+            //board[i+1][j+1] below
+            //board[i][j-1] left
+            
+            Node current = (Node)frontier.pop();
+            current.mark = true;
+            
+            /*
+            if(!current.value.equals("*") && !current.value.equals("P")){
+                current.value = ".";
+            }
+            */
+            
+            if(!current.value.equals("*")){
+                curV = current.v;
+                curH = current.h;
+                
+                if(checkViable(board, curV + 1, curH)){
+                    board[curV+1][curH].path = current.path;
+                    board[curV+1][curH].path.push(current);
+                    frontier.push(board[curV+1][curH]);
+                }
+                
+                if(checkViable(board, curV, curH + 1)){
+                    board[curV][curH+1].path = current.path;
+                    board[curV][curH+1].path.push(current);
+                    frontier.push(board[curV][curH+1]);
+                }
+                
+                if(checkViable(board, curV - 1, curH)){
+                    board[curV-1][curH].path = current.path;
+                    board[curV-1][curH].path.push(current);
+                    frontier.push(board[curV-1][curH]);
+                }
+                
+                if(checkViable(board, curV, curH - 1)){
+                    board[curV][curH-1].path = current.path;
+                    board[curV][curH-1].path.push(current);
+                    frontier.push(board[curV][curH-1]);
+                }
+            }
+            else{
+                while(!current.path.empty()){
+                    Node temp = (Node)current.path.pop();
+                    temp.value = ".";
+                }
+                
+                done = true;
+            }
+            
+            if(frontier.empty() || checker > 10000){
+                done = true;
+            }
+            
+            //done = true;
+        }
     }
         
         
@@ -98,7 +174,7 @@ public class AIProg1 {
                 }
             }
         }
-         
+        
         //h(n)
         //expand the node that has the lowest cost
         
@@ -109,17 +185,126 @@ public class AIProg1 {
         //board[i+1][j+1] below
         //board[i][j-1] left
         
+        int checker = 0;
         while(!done){
+            checker++;
+            
             board[curV][curH].mark = true;
+            if(!board[curV][curH].value.equals("P")){
+                board[curV][curH].value = ".";
+            }
             
+            Node winningNode = board[curV][curH];
+            double curValue = 99999;
+            //System.out.println("current: " + curValue);
+            double checkDist = 0;
+
+            if(curV < board.length - 1){
+                if(!board[curV + 1][curH].value.equals("%") && !board[curV + 1][curH].mark){
+                    checkDist = distance(curV + 1, curH, endV, endH);
+                    System.out.println("down: " + checkDist);
+                    if(curValue > checkDist){
+                        curValue = checkDist;
+                        winningNode = board[curV + 1][curH];
+                        //System.out.println("down");
+                    }
+                    
+                    if(curValue == checkDist){
+                        int temp = (int)(Math.random() * 10 + 1);
+                        if(temp > 5){
+                            winningNode = board[curV + 1][curH];
+                        }
+                    }
+                }
+            }
             
+            if(curH < board[0].length - 1){
+                if(!board[curV][curH + 1].value.equals("%") && !board[curV][curH + 1].mark){
+                    checkDist = distance(curV, curH + 1, endV, endH);
+                    System.out.println("right: " + checkDist);
+                    if(curValue > checkDist){
+                        curValue = checkDist;
+                        winningNode = board[curV][curH + 1];
+                        //System.out.println("right");
+                    }
+                    
+                    if(curValue == checkDist){
+                        int temp = (int)(Math.random() * 10 + 1);
+                        if(temp > 5){
+                            winningNode = board[curV][curH + 1];
+                        }
+                    }
+                }
+            }
             
-            done = true;
+            if(curV > 0){
+                if(!board[curV - 1][curH].value.equals("%") && !board[curV - 1][curH].mark){
+                    checkDist = distance(curV - 1, curH, endV, endH);
+                    System.out.println("up: " + checkDist);
+                    if(curValue > checkDist){
+                        curValue = checkDist;
+                        winningNode = board[curV - 1][curH];
+                        //System.out.println("up");
+                    }
+                    
+                    if(curValue == checkDist){
+                        int temp = (int)(Math.random() * 10 + 1);
+                        if(temp > 5){
+                            winningNode = board[curV - 1][curH];
+                        }
+                    }
+                }
+            }
+            
+            if(curH > 0){
+                if(!board[curV][curH - 1].value.equals("%") && !board[curV][curH - 1].mark){
+                    checkDist = distance(curV, curH - 1, endV, endH);
+                    System.out.println("left: " + checkDist);
+                    if(curValue > checkDist){
+                        curValue = checkDist;
+                        winningNode = board[curV][curH - 1];
+                        //System.out.println("left");
+                    }
+                                        
+                    if(curValue == checkDist){
+                        int temp = (int)(Math.random() * 10 + 1);
+                        if(temp > 5){
+                            winningNode = board[curV][curH - 1];
+                        }
+                    }
+                }
+            }
+
+            curV = winningNode.v;
+            curH = winningNode.h;
+            
+            //System.out.println("v: " + winningNode.v + " h: " + winningNode.h);
+            
+            if(board[curV][curH].value.equals("*")){
+                done = true;
+            }
+            
+            if(checker > 10000){
+                done = true;
+                //System.out.println((int)(Math.random() * 10));
+            }
+            
+            //done = true;
         }
     }
      
-    //finds euclidean distance between two points.
-    public static double distance(int sH, int sV, int eH, int eV){
+    public static boolean checkViable(Node[][] board, int v, int h){
+        if(v < board.length - 1 && h < board[0].length - 1 && v > 0 && h > 0){
+            if(!board[v][h].mark && !board[v][h].value.equals("%")){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+     
+    //finds manhattan distance between two points.
+    public static double distance(int sV, int sH, int eV, int eH){
         double output = 0;
         
         //manhattan distance
--- Node.java
+++ Node.java
@@ -5,6 +5,8 @@
  */
 package aiprog1;
 
+import java.util.Stack;
+
 /**
  *
  * @author Chance
@@ -13,14 +15,26 @@ public class Node {
     public String value; //string value stored, could be " ", %, P, or *
     public int h; //height position
     public int v; //vertical position
+    //h and v are just to make things more convenient when figuring out where the node is.
     public boolean mark;
+    //public String path; //the path so far.
+    public Stack path; //the path so far.
     
     public Node(String iValue, int iV, int iH){
         value = iValue;
         h = iH;
         v = iV;
         mark = false;
+        path = new Stack();
     }
     
-    //h and v are just to make things more convenient when figuring out where the node is.
+    //finds manhattan distance between two points.
+    public double distance(int eV, int eH){
+        double output = 0;
+        
+        //manhattan distance
+        output = Math.abs(v - eV) + Math.abs(h - eH);
+        
+        return output;
+    }
 }
